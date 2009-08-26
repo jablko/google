d6923504f16eddd402ed79ca30e3d83e1d7fb605
Jack Bates <ms419@freezone.co.uk>
2009-08-26 08:37:12 -0700

    Update regular expressions to support serial commas, http://en.wikipedia.org/wiki/Serial_comma

--- autolink.js
+++ autolink.js
@@ -15,15 +15,15 @@ $(function ()
             .replace(/</g, '&lt;')
             .replace(/>/g, '&gt;')
             .replace(/\br(\d+)\b/g, '<a class="external text" href="http://code.google.com/p/qubit-toolkit/source/detail?r=$1">$&</a>')
-            .replace(/\brevisions?\s*#?\d+(?:\s*(?:&|,|and)\s*#?\d+)+\b/gi, function ($0)
+            .replace(/\brevisions?\s+#?\d+(?:,?\s+(?:(?:&|and)\s+)?#?\d+)+\b/gi, function ($0)
               {
                 return $0.replace(/#?(\d+)/gi, '<a class="external text" href="http://code.google.com/p/qubit-toolkit/source/detail?r=$1">$&</a>');
               })
-            .replace(/\brevision\s*#?(\d+)\b/gi, '<a class="external text" href="http://code.google.com/p/qubit-toolkit/source/detail?r=$1">$&</a>')
-            .replace(/\bissues?\s*#?\d+(?:\s*(?:&|,|and)\s*#?\d+)+\b/gi, function ($0)
+            .replace(/\brevision\s+#?(\d+)\b/gi, '<a class="external text" href="http://code.google.com/p/qubit-toolkit/source/detail?r=$1">$&</a>')
+            .replace(/\bissues?\s+#?\d+(?:,?\s+(?:(?:&|and)\s+)?#?\d+)+\b/gi, function ($0)
               {
                 return $0.replace(/#?(\d+)/gi, '<a class="external text" href="http://code.google.com/p/qubit-toolkit/issues/detail?id=$1">$&</a>');
               })
-            .replace(/\bissue\s*#?(\d+)\b/gi, '<a class="external text" href="http://code.google.com/p/qubit-toolkit/issues/detail?id=$1">$&</a>'));
+            .replace(/\bissue\s+#?(\d+)\b/gi, '<a class="external text" href="http://code.google.com/p/qubit-toolkit/issues/detail?id=$1">$&</a>'));
         });
   });
