3ddf36a8df25659404dd5bba31310910e33eb0fe
Jack Bates <ms419@freezone.co.uk>
2009-08-25 17:48:46 -0700

    Add JavaScript for autolinking patterns like "revision 123" and "issue 123" to Google project hosting

---
+++ autolink.js
@@ -0,0 +1,29 @@
+$(function ()
+  {
+    // Replace text node children of all elements except <textarea>s according
+    // to regular expressions, http://dev.jquery.com/ticket/4361
+    $(':not(textarea)')
+      .contents()
+      .filter(function ()
+        {
+          return 3 == this.nodeType;
+        })
+      .each(function ()
+        {
+          $(this).replaceWith(this.nodeValue
+            .replace(/&/g, '&amp;')
+            .replace(/</g, '&lt;')
+            .replace(/>/g, '&gt;')
+            .replace(/\br(\d+)\b/g, '<a class="external text" href="http://code.google.com/p/qubit-toolkit/source/detail?r=$1">$&</a>')
+            .replace(/\brevisions?\s*#?\d+(?:\s*(?:&|,|and)\s*#?\d+)+\b/gi, function ($0)
+              {
+                return $0.replace(/#?(\d+)/gi, '<a class="external text" href="http://code.google.com/p/qubit-toolkit/source/detail?r=$1">$&</a>');
+              })
+            .replace(/\brevision\s*#?(\d+)\b/gi, '<a class="external text" href="http://code.google.com/p/qubit-toolkit/source/detail?r=$1">$&</a>')
+            .replace(/\bissues?\s*#?\d+(?:\s*(?:&|,|and)\s*#?\d+)+\b/gi, function ($0)
+              {
+                return $0.replace(/#?(\d+)/gi, '<a class="external text" href="http://code.google.com/p/qubit-toolkit/issues/detail?id=$1">$&</a>');
+              })
+            .replace(/\bissue\s*#?(\d+)\b/gi, '<a class="external text" href="http://code.google.com/p/qubit-toolkit/issues/detail?id=$1">$&</a>'));
+        });
+  });
