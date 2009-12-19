4cb8289e3092b201d0fc902c83eaaef61d5c7e8a
Jack Bates <ms419@freezone.co.uk>
2009-12-18 16:38:40 -0800

    Autolink "commit" like "revision"

--- autolink.js
+++ autolink.js
@@ -15,11 +15,11 @@ $(function ()
             .replace(/</g, '&lt;')
             .replace(/>/g, '&gt;')
             .replace(/\br(\d+)\b/g, '<a class="external text" href="http://code.google.com/p/qubit-toolkit/source/detail?r=$1">$&</a>')
-            .replace(/\brevisions?\s+#?\d+(?:,?\s+(?:(?:&|and)\s+)?#?\d+)+\b/gi, function ($0)
+            .replace(/\b(?:commit|revision)s?\s+#?\d+(?:,?\s+(?:(?:&|and)\s+)?#?\d+)+\b/gi, function ($0)
               {
                 return $0.replace(/#?(\d+)/gi, '<a class="external text" href="http://code.google.com/p/qubit-toolkit/source/detail?r=$1">$&</a>');
               })
-            .replace(/\brevision\s+#?(\d+)\b/gi, '<a class="external text" href="http://code.google.com/p/qubit-toolkit/source/detail?r=$1">$&</a>')
+            .replace(/\b(?:commit|revision)\s+#?(\d+)\b/gi, '<a class="external text" href="http://code.google.com/p/qubit-toolkit/source/detail?r=$1">$&</a>')
             .replace(/\bissues?\s+#?\d+(?:,?\s+(?:(?:&|and)\s+)?#?\d+)+\b/gi, function ($0)
               {
                 return $0.replace(/#?(\d+)/gi, '<a class="external text" href="http://code.google.com/p/qubit-toolkit/issues/detail?id=$1">$&</a>');
